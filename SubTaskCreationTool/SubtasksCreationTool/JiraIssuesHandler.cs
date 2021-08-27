17463ad359cdc7adc1ad6e7924bbcb052a33d996
Ivan Umnov <iumnov@oneinc.com>
2021-08-27 17:00:35 +0700

    Change logic for creating QaSubTask

--- SubTaskCreationTool/SubtasksCreationTool/JiraIssuesHandler.cs
+++ SubTaskCreationTool/SubtasksCreationTool/JiraIssuesHandler.cs
@@ -24,10 +24,10 @@ namespace SubtasksCreationTool
         private const int HoursInPu = 6;
 
         private readonly IJiraClient _jiraClient;
-        
+
         private JiraUser IlyaShalin = new JiraUser()
             {Id = "557058:1dc64da9-9977-495c-a7f9-40e7f82b7266", Name = "Ilya Shalin"};
-        
+
         private Dictionary<RequestType, string> _requests = new Dictionary<RequestType, string>()
         {
             {
@@ -49,36 +49,36 @@ namespace SubtasksCreationTool
             {
                 var issue = issueDto.Issue;
                 var typeOfSubtasksToCreate = issueDto.TypeOfSubtasksToCreate;
-                
+
                 await ResetTheIssueOriginalEstimate(issue);
 
                 if (typeOfSubtasksToCreate.Count == 0)
                 {
                     continue;
                 }
-                
+
                 string mainTaskSummary = issue.Fields.Summary;
                 string projectKey = issue.Fields.Project.Key;
                 string issueKey = issue.Key;
                 int issueId = issue.Id;
-                
+
                 bool changedTechReviewStatus = false;
-                
+
                 if (issue.Fields.Status.Name == OpenStatus)
                 {
                     await _jiraClient.TransitIssueWithFieldsAsync(issueId, TechnicalAnalysisNeededIdFromOpen,
-                        new IssueFields { Customer = NaCustomer });
+                        new IssueFields {Customer = NaCustomer});
                 }
                 else if (issue.Fields.Status.Name == TechAnalysisReviewStatus)
                 {
                     changedTechReviewStatus = true;
-                    
+
                     await _jiraClient.TransitIssueWithFieldsAsync(issueId, TechnicalAnalysisId,
-                        new IssueFields { Customer = NaCustomer });
+                        new IssueFields {Customer = NaCustomer});
                 }
 
                 string titleDevTask = "[Dev] Implementation of ";
-                
+
                 foreach (var typeOfSubtask in typeOfSubtasksToCreate)
                 {
                     if (typeOfSubtask == QaSubTaskType)
@@ -89,10 +89,13 @@ namespace SubtasksCreationTool
                     }
                     else
                     {
-                        if (mainTaskSummary.Contains("implement", StringComparison.OrdinalIgnoreCase) || mainTaskSummary.Contains("[Dev]", StringComparison.OrdinalIgnoreCase) || mainTaskSummary.Contains("investigat", StringComparison.OrdinalIgnoreCase))
+                        if (mainTaskSummary.Contains("implement", StringComparison.OrdinalIgnoreCase) ||
+                            mainTaskSummary.Contains("[Dev]", StringComparison.OrdinalIgnoreCase) ||
+                            mainTaskSummary.Contains("investigat", StringComparison.OrdinalIgnoreCase))
                         {
                             titleDevTask = string.Empty;
                         }
+
                         Console.WriteLine($"Creating a Dev-subtask for {issueKey}");
                         await CreateSubtask(issueKey, $"{titleDevTask}{mainTaskSummary}",
                             issue.Fields.DevPreliminaryUnits.Value, SubTaskType, projectKey);
@@ -109,29 +112,21 @@ namespace SubtasksCreationTool
         public IEnumerable<IssueDto> GetTypeOfSubtasksToCreate(IEnumerable<Issue> issues)
         {
             List<IssueDto> result = new List<IssueDto>();
-            
+
             foreach (var issue in issues)
             {
-                var typeOfSubtasksToCreate =  new string[] { QaSubTaskType, SubTaskType }.ToList();
+                var typeOfSubtasksToCreate = new string[] {QaSubTaskType, SubTaskType}.ToList();
                 var existingTypeOfSubtasks = issue.Fields.Subtasks.Select(i => i.Fields.IssueType.Name).ToList();
                 typeOfSubtasksToCreate = typeOfSubtasksToCreate.Except(existingTypeOfSubtasks).ToList();
 
-                if (issue.Fields.IssueType.Name == InternalTechnicalTaskType)
-                {
-                    if (issue.Fields.QaPreliminaryUnits == null || issue.Fields.QaPreliminaryUnits == 0)
-                    {
-                        typeOfSubtasksToCreate.Remove(QaSubTaskType);
-                    }
-                }
-                
-                if (issue.Fields.QaPreliminaryUnits.HasValue && issue.Fields.QaPreliminaryUnits == 0)
+                if (issue.Fields.QaPreliminaryUnits == null || issue.Fields.QaPreliminaryUnits == 0)
                 {
                     typeOfSubtasksToCreate.Remove(QaSubTaskType);
                 }
-                
-                result.Add(new IssueDto { Issue = issue, TypeOfSubtasksToCreate = typeOfSubtasksToCreate });
+
+                result.Add(new IssueDto {Issue = issue, TypeOfSubtasksToCreate = typeOfSubtasksToCreate});
             }
-            
+
             return result;
         }
 
@@ -143,18 +138,17 @@ namespace SubtasksCreationTool
                 {
                     if (issue.Fields.ResponsibleDev != null)
                     {
-                        Console.WriteLine($"Sending mention to {issue.Fields.ResponsibleDev.Name} about adding Dev PU for {issue.Key}...");
+                        Console.WriteLine(
+                            $"Sending mention to {issue.Fields.ResponsibleDev.Name} about adding Dev PU for {issue.Key}...");
                         await CreateComment(issue.Key, issue.Fields.ResponsibleDev, RequestType.AddPU);
                     }
                 }
 
-                if (issue.Fields.IssueType.Name == TaskType)
+                if (issue.Fields.QaPreliminaryUnits == null)
                 {
-                    if (issue.Fields.QaPreliminaryUnits == null)
-                    {
-                        Console.WriteLine($"Sending mention to {IlyaShalin.Name} about adding Dev PU for {issue.Key}...");
-                        await CreateComment(issue.Key, IlyaShalin, RequestType.AddPU);
-                    }
+                    Console.WriteLine(
+                        $"Sending mention to {IlyaShalin.Name} about adding Dev PU for {issue.Key}...");
+                    await CreateComment(issue.Key, IlyaShalin, RequestType.AddPU);
                 }
             }
         }
@@ -172,12 +166,13 @@ namespace SubtasksCreationTool
                     {
                         if (issue.Fields.ResponsibleDev != null)
                         {
-                            Console.WriteLine($"Send mention to {issue.Fields.ResponsibleDev.Name} about updating Dev PU or estimates for {issue.Key}...");
+                            Console.WriteLine(
+                                $"Send mention to {issue.Fields.ResponsibleDev.Name} about updating Dev PU or estimates for {issue.Key}...");
                             await CreateComment(issue.Key, issue.Fields.ResponsibleDev, RequestType.UpdatePu);
                         }
                     }
                 }
-                
+
                 var existingQaSubtasks = issue.Fields.Subtasks.Where(i => i.Fields.IssueType.Name == QaSubTaskType);
                 if (existingQaSubtasks.Any())
                 {
@@ -185,14 +180,15 @@ namespace SubtasksCreationTool
                         .Sum();
                     if (sumEstimate != (int) Math.Truncate(issue.Fields.QaPreliminaryUnits.Value * HoursInPu * 3600))
                     {
-                        Console.WriteLine($"Send mention to {IlyaShalin.Name} about updating QA PU or estimates for {issue.Key}...");
+                        Console.WriteLine(
+                            $"Send mention to {IlyaShalin.Name} about updating QA PU or estimates for {issue.Key}...");
                         await CreateComment(issue.Key, IlyaShalin, RequestType.UpdatePu);
                     }
                 }
             }
         }
-        
-        private async Task  CreateComment(string issueKey, JiraUser user, RequestType requestType)
+
+        private async Task CreateComment(string issueKey, JiraUser user, RequestType requestType)
         {
             await _jiraClient.AddCommentAsync(issueKey, new Comment()
             {
@@ -215,7 +211,7 @@ namespace SubtasksCreationTool
                                 },
                                 new TextNode()
                                 {
-                                    Text = $", {_requests[requestType]}" 
+                                    Text = $", {_requests[requestType]}"
                                 }
                             }
                         }
@@ -223,12 +219,13 @@ namespace SubtasksCreationTool
                 }
             });
         }
-        
+
         private static string GetOriginalEstimateToString(double originalEstimate)
         {
             double originalEstimateInDouble = HoursInPu * originalEstimate;
-            int originalEstimateHours = (int)Math.Truncate(originalEstimateInDouble);
-            double originalEstimateMinutes = (int)Math.Truncate((originalEstimateInDouble - originalEstimateHours) * 60);
+            int originalEstimateHours = (int) Math.Truncate(originalEstimateInDouble);
+            double originalEstimateMinutes =
+                (int) Math.Truncate((originalEstimateInDouble - originalEstimateHours) * 60);
             return $"{originalEstimateHours}h {originalEstimateMinutes}m";
         }
 
@@ -236,7 +233,12 @@ namespace SubtasksCreationTool
         {
             if (issue.Fields.TimeTracking.OriginalEstimate != "0h")
             {
-                await _jiraClient.UpdateIssueAsync(issue.Key, new Issue{Fields = new IssueFields{TimeTracking = new TimeTracking {OriginalEstimate = "0h", TimeRemaining = "0h"}}});
+                await _jiraClient.UpdateIssueAsync(issue.Key,
+                    new Issue
+                    {
+                        Fields = new IssueFields
+                            {TimeTracking = new TimeTracking {OriginalEstimate = "0h", TimeRemaining = "0h"}}
+                    });
             }
         }
 
@@ -248,7 +250,7 @@ namespace SubtasksCreationTool
             builderTask.WithParentIssue(mainTaskKey);
             string timeEstimate = GetOriginalEstimateToString(mainTaskPreliminaryUnits);
             builderTask.WithOriginalEstimate(timeEstimate);
-            
+
             await builderTask.CreateAsync();
         }
 
